0b9fe88409c1f4fcac95ee95b7a13b056115ab77
Roberto Castellor <roberto.castellor@gmail.com>
2022-04-04 19:10:07 +0200

    Initial commit

---
+++ src/main/java/es/castellor/testeate/backend/webapp/TesteateBackendWebappApplication.java
@@ -0,0 +1,13 @@
+package es.castellor.testeate.backend.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteateBackendWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteateBackendWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/castellor/testeate/backend/webapp/TesteateBackendWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package es.castellor.testeate.backend.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteateBackendWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
